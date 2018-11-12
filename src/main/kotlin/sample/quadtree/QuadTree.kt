eade7ed8a2c026239b28275285174259979cdcaa
Loic Gardaire <loic.gardaire@gmail.com>
2018-11-12 10:52:47 +0100

    first idea of quadtree

--- src/main/kotlin/sample/quadtree/QuadTree.kt
+++ src/main/kotlin/sample/quadtree/QuadTree.kt
@@ -2,58 +2,50 @@ package sample.quadtree
 
 import sample.Segment
 
-class QuadTree(
-        xCenter : Int,
-        yCenter : Int,
-        halfDimmension : Int
-){
+class QuadTree(var xCenter: Int, var yCenter: Int, halfDimmension: Int) {
     var QT_NODE_CAPACITY = 4
 
-    var boundary : Box = Box(xCenter, yCenter, halfDimmension)
+    var boundary: Box? = null
 
-    val segments : MutableList<Segment> = mutableListOf()
+    val segments: MutableList<Segment> = mutableListOf()
 
-    // Enfants
-    var half = halfDimmension/2
-    var northWest : QuadTree = QuadTree(xCenter - half, yCenter + half, half)
-    var northEast : QuadTree = QuadTree(xCenter + half, yCenter + half, half)
-    var southWest : QuadTree = QuadTree(xCenter - half, yCenter - half, half)
-    var southEast : QuadTree = QuadTree(xCenter + half, yCenter - half, half)
+    var halfDimension = halfDimmension
 
+    var northWest: QuadTree? = null
+    var northEast: QuadTree? = null
+    var southWest: QuadTree? = null
+    var southEast: QuadTree? = null
 
+    init {
+        this.boundary = Box(xCenter, yCenter, halfDimension)
+    }
 
-    fun insert(segment: Segment) : Boolean {
-        // Ignorer les objets qui n'appartiennent pas à ce quadtree
-        if (!boundary.containsSegment(segment))
-            return false  // l'objet ne doit pas être ajouté
+    fun insert(segment: Segment): Boolean {
+        if (!boundary!!.containsSegment(segment))
+            return false
 
-        // S'il reste de la place dans ce quadtree, y ajouter l'objet
-        if (segments.size < QT_NODE_CAPACITY)
-        {
+        if (segments.size < QT_NODE_CAPACITY) {
             segments.add(segment)
             return true
         }
 
-        // Sinon, subdiviser le quadtree, puis ajouter le point au nœud qui l'acceptera
         if (northWest == null)
             subdivide()
 
-        if (northWest.insert(segment)) return true
-        if (northEast.insert(segment)) return true
-        if (southWest.insert(segment)) return true
-        if (southEast.insert(segment)) return true
+        if (northWest!!.insert(segment)) return true
+        if (northEast!!.insert(segment)) return true
+        if (southWest!!.insert(segment)) return true
+        if (southEast!!.insert(segment)) return true
 
-        // Sinon, le point ne peut être inséré, pour une raison inconnue (cela ne devrait jamais arriver)
         return false
     }
 
-    fun queryRange(range : Box) : MutableList<Segment>
-    {
+    fun queryRange(range: Box): MutableList<Segment> {
         // Préparer le tableau de résultats
-        val pointsInRange : MutableList<Segment> = mutableListOf()
+        val pointsInRange: MutableList<Segment> = mutableListOf()
 
         // Tout interrompre si la «zone de recherche» n'intersecte pas ce quadrant
-        if (!boundary.intersectsWithOtherBox(range))
+        if (!boundary!!.intersectsWithOtherBox(range))
             return pointsInRange // liste vide
 
         // Vérifier les objets à ce niveau de quadrant
@@ -67,15 +59,19 @@ class QuadTree(
             return pointsInRange
 
         // Sinon, relancer la recherche sur les 4 enfants et ajouter les points trouvés
-        pointsInRange.addAll(northWest.queryRange(range))
-        pointsInRange.addAll(northEast.queryRange(range))
-        pointsInRange.addAll(southWest.queryRange(range))
-        pointsInRange.addAll(southEast.queryRange(range))
+        pointsInRange.addAll(northWest!!.queryRange(range))
+        pointsInRange.addAll(northEast!!.queryRange(range))
+        pointsInRange.addAll(southWest!!.queryRange(range))
+        pointsInRange.addAll(southEast!!.queryRange(range))
 
         return pointsInRange
     }
 
-    fun subdivide(){
-
+    fun subdivide() {
+        val half = halfDimension / 2
+        northWest = QuadTree(xCenter - half, yCenter + half, half)
+        northEast = QuadTree(xCenter + half, yCenter + half, half)
+        southWest = QuadTree(xCenter - half, yCenter - half, half)
+        southEast = QuadTree(xCenter + half, yCenter - half, half)
     }
 }
